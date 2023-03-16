8871c0d86175bb100ba3828e1b3f358c95cd7c72
Dharinichib <127197788+Dharinichib@users.noreply.github.com>
2023-03-16 23:34:28 +0530

    Delete odd_even.js

--- odd_even.js
+++
@@ -1,9 +0,0 @@
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const odds = arr.filter((number => {
-    return number % 2 !== 0;}));
-const evens = arr.filter(number => {
-    return number % 2 === 0;
-  });
-
-console.log("EVEN NUMBER ARE: "+evens);
-console.log("ODD NUMBERS ARE: "+odds)
